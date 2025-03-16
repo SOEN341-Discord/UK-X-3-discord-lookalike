9c539685d84714439f9f863665871d68135f6c05
Maëlys <ma_arna@live.concordia.ca>
2025-03-16 00:30:16 -0400

    Revert "Modified Profile Page for Update Profile"
    This reverts commit bfb3613c1625f1fd3b794922bddc761d9d2cd5f7.

--- NewCode/chat-app/resources/views/layouts/navigation.blade.php
+++ NewCode/chat-app/resources/views/layouts/navigation.blade.php
@@ -51,7 +51,7 @@
             <div class="p-4">
                 <x-dropdown align="bottom" width="48">
                     <x-slot name="trigger">
-                        <button class="inline-flex items-center px-3 py-2 border border-transparent text-m leading-4 font-medium rounded-md text-gray-300 bg-gray-800 hover:text-white focus:outline-none transition ease-in-out duration-150">
+                        <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-300 bg-gray-800 hover:text-white focus:outline-none transition ease-in-out duration-150">
                             <div>{{ Auth::user()->name }}</div>
                             <div class="ms-1">
                                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
